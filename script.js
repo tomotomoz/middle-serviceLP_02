adcf35490d63ef500dd2625f9b3f0c1cc6240e41
tomotomoz <tomoko.essvivh@gmail.com>
2024-11-19 22:52:43 +0900

    init

---
+++ script.js
@@ -0,0 +1,57 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  // FAQ テキストの表示
+  const faqTitles = document.querySelectorAll('.faq__title');
+  faqTitles.forEach((faqTitle) => {
+    faqTitle.addEventListener('click', (e) => {
+      e.preventDefault();
+      const content = faqTitle.nextElementSibling;
+      faqTitle.classList.toggle('active');
+      content.classList.toggle('open');
+    });
+  });
+
+  const hamburger = document.querySelector(".header__hamburger");
+  const navArea = document.querySelector('.header__navAreaWrapper')
+  const nav = document.querySelector('u-BG')
+
+  hamburger.addEventListener('click',function(){
+    navArea.classList.toggle('hamburgerOpen')
+  nav.classList.toggle('navOpen')
+  })
+
+
+
+  // **Swiper の初期化**
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal', 
+    loop: true,       
+    speed: 1000,             
+    slidesPerView: 5, // 1度に表示するスライド枚数
+  slidesPerGroup: 1, // 1度に移動するスライド枚数  
+    loopAdditionalSlides: 2,
+    autoplay: {
+      delay: 3000,          
+      disableOnInteraction: false, 
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    breakpoints: {
+      1000:{
+        slidesPerView: 4, 
+        slidesPerGroup: 1, 
+      },
+      720: {
+        slidesPerView: 3, 
+        slidesPerGroup: 1, 
+      },
+      430: {
+        slidesPerView: 2, 
+        slidesPerGroup: 1, 
+      },
+    },
+  });
+});
